519e74d4ff7bbb9fa02f259deb3161fb2543af40
HLTienDat <hltiendat2002@gmail.com>
2024-07-19 14:39:01 +0700

    Initialize

---
+++ server/app.ts
@@ -0,0 +1,26 @@
+require('dotenv').config();
+import express,{Request,Response,NextFunction} from "express";
+export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+// body parser
+app.use(express.json({limit: "50mb"}));
+// cookie parser
+app.use(cookieParser());
+
+app.use(cors({
+    origin: process.env.ORIGIN
+}));
+
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        succcess: true,
+        message: "API is working",
+    });
+});
+
+app.get("*", (req: Request, res: Response, next: NextFunction) => {
+    const err = new Error(`Route ${req.originalUrl} not found`) as any; 
+    err.statusCode = 404;
+    next(err);
+});
