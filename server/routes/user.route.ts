349e6d525457e9f1f167145c65e46f377def87b5
PhanTanCuong <phantancuong2002@gmail.com>
2024-09-25 20:38:06 +0700

    success activation user

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,9 +1,12 @@
 import express from 'express';
-import {registrationUser} from '../controllers/user.controller';
+import { activateUser, registrationUser } from '../controllers/user.controller';
 
 const userRouter = express.Router();
 
 //navigation 
-userRouter.post('/registration',registrationUser);
+userRouter.post('/registration', registrationUser);
+
+userRouter.post('/activate-user', activateUser);
+
 
 export default userRouter;
