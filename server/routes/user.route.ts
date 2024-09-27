a9cffb9f2c376ad60c0232ee7a7687824e7aa578
PhanTanCuong <phantancuong2002@gmail.com>
2024-09-27 21:00:13 +0700

    Auth
    error not fix yet

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,5 +1,12 @@
 import express from 'express';
-import { activateUser, registrationUser, loginUser, logoutUser } from '../controllers/user.controller';
+import {
+    activateUser,
+    registrationUser,
+    loginUser,
+    logoutUser
+} from '../controllers/user.controller';
+
+import {isAutheticated} from '../middleware/auth';
 
 const userRouter = express.Router();
 
@@ -10,7 +17,7 @@ userRouter.post('/activate-user', activateUser);
 
 userRouter.post('/login', loginUser);
 
-userRouter.post('/logout', logoutUser);
+userRouter.post('/logout', isAutheticated,logoutUser);
 
 
 export default userRouter;
