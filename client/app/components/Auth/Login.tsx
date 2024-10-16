ef192d77c4af6db86c6227dcc78fd86931f1a391
PhanTanCuong <phantancuong2002@gmail.com>
2024-10-16 20:32:01 +0700

    Login modal
    Done

--- client/app/components/Auth/Login.tsx
+++ client/app/components/Auth/Login.tsx
@@ -22,7 +22,7 @@ const schema = Yup.object().shape({
   password: Yup.string().required("Please enter your password").min(6),
 });
 
-const Login: Fc<Props> = (props: Props) => {
+const Login: FC<Props> = (prop:Props) => {
   const [show, setShow] = useState(false);
 
   const formik = useFormik({
@@ -33,7 +33,6 @@ const Login: Fc<Props> = (props: Props) => {
     validationSchema: schema,
     onSubmit: async ({ email, password }) => {
       console.log(email, password);
-      props.setRoute("/dashboard");
     },
   });
 
@@ -60,35 +59,61 @@ const Login: Fc<Props> = (props: Props) => {
         {errors.email && touched.email && (
           <span className="text-red-500 pt-2 block">{errors.email}</span>
         )}
-        <div className="w-full mt-5 relative mb-1"></div>
-        <label htmlFor="password" className={`${styles.label}`}>
-          Enter your password
-        </label>
-        <input
-          type={!show ? "password" : "text"}
-          name="password"
-          value={values.password}
-          onChange={handleChange}
-          id="password"
-          placeholder="password!@%"
-          className={`${
-            errors.password && touched.password && "border-red-500"
-          } ${styles.input}`}
-        />
-        {!show ? (
-          <AiOutlineEyeInvisible
-            className="absolute bottom-6 right-5   z-1 cursor-pointer text-black dark:text-white bg-transparent"
-            size={20}
-            onClick={() => setShow(true)}
+        <div className="w-full mt-5 relative mb-1">
+          <label htmlFor="password" className={`${styles.label}`}>
+            Enter your password
+          </label>
+          <input
+            type={!show ? "password" : "text"}
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            id="password"
+            placeholder="password!@%"
+            className={`${
+              errors.password && touched.password && "border-red-500"
+            } ${styles.input}`}
           />
-        ) : (
-          <AiOutlineEye
-            className="absolute bottom-6 right-5   z-1 cursor-pointer text-black dark:text-white bg-transparent"
-            size={20}
-            onClick={() => setShow(false)}
-      
-          />
-        )}
+          {!show ? (
+            <AiOutlineEyeInvisible
+              className="absolute bottom-3 right-2   z-1 cursor-pointer text-black dark:text-white bg-transparent"
+              size={20}
+              onClick={() => setShow(true)}
+            />
+          ) : (
+            <AiOutlineEye
+              className="absolute bottom-3 right-2   z-1 cursor-pointer text-black dark:text-white bg-transparent"
+              size={20}
+              onClick={() => setShow(false)}
+            />
+          )}
+          {errors.password && touched.password && (
+            <span className="text-red-500 pt-2 block">{errors.password}</span>
+          )}
+        </div>
+        <div className="w-full mt-5">
+          <input type="submit" value="Login" className={`${styles.button}`} />
+        </div>
+        <br />
+        <h5 className="text-center pt-4 font-Poppins text-[14px] text-black dark:text-white">
+          Or join us
+        </h5>
+        <div className="flex items-center justify-center my-3">
+          <FcGoogle size={30} className="cursor-pointer mr-2"></FcGoogle>
+          <AiFillGithub
+            size={30}
+            className="cursor-pointer ml-2"
+          ></AiFillGithub>
+        </div>
+        <h5 className="text-center pt-4 font-Poppins text-[14px]">
+          Not have any account ?{""}
+          <span
+            className="text-[#2190ff] pl-1 cursor-pointer"
+            onClick={() => setRoute("Sign-up")}
+          >
+            Sign up
+          </span>
+        </h5>
       </form>
     </div>
   );
